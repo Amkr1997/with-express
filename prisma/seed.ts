b8100a534ac7d9d91e346a6b6d0f66fca05cfe58
aman khare <aman.khare@geekyants.com>
2025-10-15 13:49:24 +0530

    feat: Created multiple crud operations to db with prisma client.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,10 +2,87 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function seed() {
+  // await prisma.user.deleteMany();
+
   await prisma.user.createMany({
     data: [
-      { name: "John Doe", email: "a@b.com" },
-      { name: "Jane Doe", email: "a@c.com" },
+      {
+        name: "John Doe",
+        email: "a@b.com",
+        age: 20,
+        isMarried: false,
+        nationality: "American",
+      },
+      {
+        name: "Jane Doe",
+        email: "a@c.com",
+        age: 21,
+        isMarried: true,
+        nationality: "Canadian",
+      },
+      {
+        name: "Jim Doe",
+        email: "a@d.com",
+        age: 22,
+        isMarried: false,
+        nationality: "British",
+      },
+      {
+        name: "Jill Doe",
+        email: "a@e.com",
+        age: 23,
+        isMarried: true,
+        nationality: "Australian",
+      },
+      {
+        name: "Jack Doe",
+        email: "a@f.com",
+        age: 24,
+        isMarried: false,
+        nationality: "New Zealander",
+      },
+      {
+        name: "Jill Doe",
+        email: "a@g.com",
+        age: 25,
+        isMarried: true,
+        nationality: "Indian",
+      },
+      {
+        name: "Jill Doe",
+        email: "a@h.com",
+        age: 26,
+        isMarried: false,
+        nationality: "Irish",
+      },
+      {
+        name: "Jill Doe",
+        email: "a@i.com",
+        age: 27,
+        isMarried: true,
+        nationality: "Scottish",
+      },
+      {
+        name: "Jill Doe",
+        email: "a@j.com",
+        age: 28,
+        isMarried: false,
+        nationality: "Welsh",
+      },
+      {
+        name: "Jill Doe",
+        email: "a@k.com",
+        age: 29,
+        isMarried: true,
+        nationality: "Scottish",
+      },
+      {
+        name: "Jill Doe",
+        email: "a@l.com",
+        age: 30,
+        isMarried: false,
+        nationality: "Welsh",
+      },
     ],
   });
 }
--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,75 @@ app.get("/", (req, res) => {
 app.get("/api/users", async (_, res) => {
   try {
     const allUsers = await prisma.user.findMany();
+
+    // const allUsers = await prisma.user.findMany({
+    //   where: { isMarried: true, age: { gt: 25 } },
+    // });
+
+    // using OR operator
+    // const allUsers = await prisma.user.findMany({
+    //   where: { OR: [{ nationality: "Canadian" }, { nationality: "American" }] },
+    // });
+
+    // using AND operator with gte operator
+    // const allUsers = await prisma.user.findMany({
+    //   where: {
+    //     AND: [{ nationality: "Canadian" }, { age: { gte: 20 } }],
+    //   },
+    // });
+
+    // using NOT operator
+    // const allUsers = await prisma.user.findMany({
+    //   where: { NOT: { nationality: "American" } },
+    // });
+
+    // // using LIKE operator
+    // const allUsers = await prisma.user.findMany({
+    //   where: { name: { contains: "John" } },
+    // });
+
+    // using IN operator
+    // const allUsers = await prisma.user.findMany({
+    //   where: { nationality: { in: ["Canadian", "American"] } },
+    // });
+
+    // using findUnique fetch single user
+    // const singleUser = await prisma.user.findUnique({
+    //   where: { email: "a@b.com" },
+    // });
+
     return res.status(200).json(allUsers);
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.put("/api/users", async (_, res) => {
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { email: "a@b.com" },
+      data: {
+        name: "Aman Khare",
+        isMarried: false,
+        age: 27,
+        nationality: "Indian",
+      },
+    });
+
+    return res.status(200).json(updatedUser);
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.delete("/api/users", async (_, res) => {
+  try {
+    // delete single user
+    const deletedUser = await prisma.user.delete({
+      where: { email: "a@c.com" },
+    });
+
+    return res.status(200).json(deletedUser);
   } catch (error) {
     return res.status(500).json({ error: "Internal Server Error" });
   }
